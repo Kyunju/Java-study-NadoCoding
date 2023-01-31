611925ba55d5388a5c4ba0467798fb6a21f7dda8
Kyungju <wwmix@naver.com>
2023-01-31 23:32:21 +0900

    람다식

---
+++ src/chap_10/_03_Lambda.java
@@ -0,0 +1,33 @@
+package chap_10;
+
+public class _03_Lambda {
+    public static void main(String[] args) {
+        // 람다식
+        // 접근제어자가 필요 없음, 리턴 타입도 필요 없음
+        // (전달값1, 전달값2, ...) -> { 코드 }
+        // 자바스크립트의 arrow function 이랑 비슷하네
+    }
+
+//    public void print() {
+//        String s = "test";
+//        System.out.println(s);
+//    }
+//    () -> {
+//        String s = "test";
+//        System.out.println(s);
+//    }
+
+//    public void print (String s) {
+//        System.out.println(s);
+//    }
+
+    // 생략가능
+//    s -> System.out.println(s)
+
+//    public int add(int x, int y) {
+//        return x + y;
+//    }
+    // 자동으로 연산된 것을 리턴
+//    ( x,  y) -> x + y
+
+}
