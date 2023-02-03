0271b62623356cc274f43e15617cca6b4a7c98e1
Kyungju <wwmix@naver.com>
2023-02-04 06:01:13 +0900

    스트림 추가내용

--- src/chap_10/_05_Stream.java
+++ src/chap_10/_05_Stream.java
@@ -3,6 +3,7 @@ package chap_10;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -12,6 +13,7 @@ public class _05_Stream {
         // 아주 많은 데이터에서 원하는 조건에 따라서 1,2,3 .. 차 필터링을 할 수 있음
         // 데이터에서 필요한 요소만 가져올 수 있음
         // 데이터 가공이 편리해짐
+        // 스트림은 가공하고 나면 가공된 데이터 상태이기 때문에 다시 만들어야함
 
         // 스트림 생성
         // 배열 Arrays.stream
@@ -53,5 +55,56 @@ public class _05_Stream {
         // 90 점 이상인 점수들을 정렬
         Arrays.stream(scores).filter(x -> x >= 90).sorted().forEach(System.out::println);
         System.out.println("--------------------------------");
+
+        //"python", "java", "javascript", "c", "c++", "c#"
+        // c 로 시작하는 프로그래밍 언어
+        Arrays.stream(langs).filter(x -> x.startsWith("c")).forEach(System.out::println);
+        System.out.println("--------------------------------");
+
+        // java 라는 글자를 포함하는 언어
+        Arrays.stream(langs).filter(x -> x.contains("java")).forEach(System.out::println);
+        System.out.println("--------------------------------");
+
+        // 4글자 이하의 언어를 정렬해서 출력
+        langList.stream().filter(x -> x.length() <= 4).sorted().forEach(System.out::println);
+        System.out.println("--------------------------------");
+
+        // 4글자 이하의 언어 중에서 c 라는 글자를 포함하는 언어
+        langList.stream().filter(x -> x.length() <= 4).filter(x->x.contains("c")).forEach(System.out::println);
+        System.out.println("--------------------------------");
+
+        // 4글자 이하의 언어 중에서 c 라는 글자를 포함하는 언어가 하나라도 있는 지 여부
+        boolean anyMatch = langList.stream().filter(x -> x.length() <= 4).anyMatch(x -> x.contains("c"));
+        System.out.println(anyMatch);
+        System.out.println("--------------------------------");
+
+        // 4글자 이하의 언어들은 모두 c 라는 글자를 포함하는지 여부
+        boolean allMatch = langList.stream().filter(x -> x.length() <= 4).allMatch(x -> x.contains("c"));
+        System.out.println(allMatch);
+        System.out.println("--------------------------------");
+
+        // 4글자 이하의 언어 중에서 c 라는 글자를 포함하는 언어 뒤에 (어려워요) 라는 글자를 함께 출력
+        // map
+        langList.stream()
+                .filter(x -> x.length() <= 4)
+                .filter(x -> x.contains("c"))
+                .map(x -> x+ " (어려워요)")
+                .forEach(System.out::println);
+        System.out.println("--------------------------------");
+
+        // c 라는 글자를 포함하는 언어를 대문자로 출력
+        langList.stream()
+                .filter(x -> x.contains("c"))
+                .map(String::toUpperCase)
+                .forEach(System.out::println);
+        System.out.println("--------------------------------");
+
+        // c 라는 글자를 포함하는 언어를 대문자로 변경하여 리스트로 저장
+        List<String> langListStartsWithC = langList.stream()
+                .filter(x -> x.contains("c"))
+                .map(String::toUpperCase)
+                .collect(Collectors.toList());
+
+        langListStartsWithC.stream().forEach(System.out::println);
     }
 }
